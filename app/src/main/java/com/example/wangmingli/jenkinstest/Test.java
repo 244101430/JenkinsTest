2340a4099aeabb79b0e1c6227fb801c0632ce13e
wml8743 <wangmingli@noah-fund.com>
2018-01-02 13:52:01 +0800

    test

---
+++ app/src/main/java/com/example/wangmingli/jenkinstest/Test.java
@@ -0,0 +1,17 @@
+package com.example.wangmingli.jenkinstest;
+
+/**
+ * Created by wml8743 on 2018/1/2.
+ */
+
+public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
